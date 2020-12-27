ae6720c6d5d4cbd13f3354c810d54b7efe7b4491
Alejandro Herrera <a.herrera.turra@gmail.com>
2020-12-27 13:37:54 -0300

    [edit] Header component add button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import '../styles/components/header.css';
 
 const Header = () => (
   <header className="header global-padding">
-    <h1 className="header__title reset-margin">Mis Tareas</h1>
+    <Link to="/" className="header__link">
+      <h1 className="header__title reset-margin">Mis Tareas</h1>
+    </Link>
   </header>
 );
 
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import '../styles/components/tasks.css';
 import Task from './Task';
 import Search from './Search';
@@ -7,9 +8,14 @@ import Filters from './Filters';
 const Tasks = ({ tasks }) => (
   <div className="tasks">
     <div className="tasks__container__header">
-      <h1 className="tasks__title">Lista de tareas:</h1>
-      <div className="tasks__quantity">
-        <p>{tasks.length}</p>
+      <div className="tasks__container__header--column">
+        <div className="tasks__quantity">
+          <p>{tasks.length}</p>
+        </div>
+        <h1 className="tasks__title">Lista de tareas</h1>
+      </div>
+      <div className="tasks__container__header--column">
+        <Link to="/addtask" className="tasks__container__header--button">Agregar tarea</Link>
       </div>
     </div>
 
